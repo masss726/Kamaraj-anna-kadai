933df9a2e181a97292c918c1b14b362e022c3b5c
masss726 <nmasilamani615@gmail.com>
2025-04-10 10:05:47 +0530

    Start : add products

--- Thamilan_Maligai/src/main/java/com/example/MasilaMainController/MainController.java
+++ Thamilan_Maligai/src/main/java/com/example/MasilaMainController/MainController.java
@@ -2,15 +2,32 @@ package com.example.MasilaMainController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 	 
 	
 	@GetMapping("/index")
-	public String home() {
-	    
+	public String home() {	    
 		return "home"; 
+	}
+	@GetMapping("/Purchasing")
+	public String PurchasingPage() {
+		return "Product_Selection";
+		
+	}
+	
+	@PostMapping("/Purchasing")
+	public String PurchasingPagePost() {
+		return "Product_Selection";
+		
+	}
+	
+	@PostMapping("/GoBilling")
+	public String GoBilling() {
+		return "billing";
+		
 	}
 
 
---
+++ Thamilan_Maligai/src/main/java/com/example/model/billing.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import java.io.Serializable;
+
+public class billing implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int BillingID;
+	private String CostomerName;
+	private String MobileNo;
+	private String Date;
+	private String time;
+	private int rate;
+	private int Quantity;
+	
+
+}
